294d87a6524a77b514c2c596f0f68832914e7083
Hretu <ritudhakal5@gmail.com>
2019-05-02 10:42:17 +0800

    New application server initialised i.e express

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log('Index 2nd');
-console.log('Index is running');
+// console.log('Index 2nd');
+// console.log('Index is running');
+
+var express = require('express');//used to run multiple applications...
+var application = new express();
+
+application.listen(3001);//server name
+
+
